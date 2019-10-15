58be7ebad6a4620c587b6cf9f28fc248e0ffb3b9
Lutfi Ramadhan <lutfiramadan@gmail.com>
2019-10-15 21:44:27 +0700

    First Commit

---
+++ src/latihan31/Latihan31.java
@@ -0,0 +1,37 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan31;
+
+/**
+ *
+ * @author lutfi
+ * NAMA  : LUTFI RAMADHAN
+ * NIM   : 10118908
+ * KELAS : IF 11 KARYAWAN
+ */
+public class Latihan31 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa mhs = new Mahasiswa();
+        mhs.nim = "10110269";
+        mhs.nama = "Rizki Adam Kurniawan";
+        mhs.perkenalanDiri();
+        mhs.nim = "10110270";
+        mhs.nama = "Indra Tiola";
+        mhs.perkenalanDiri();
+        mhs.nim = "10110271";
+        mhs.nama = "Robi Tanzil Ganefi";
+        mhs.perkenalanDiri();
+        mhs.nim = "10110269";
+        mhs.nama = "Muhammad Nur Awaludin";
+        mhs.perkenalanDiri();
+    }
+    
+}
